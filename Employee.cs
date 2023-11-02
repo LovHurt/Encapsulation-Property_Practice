57247d55f2ca7bb1c24e5dc9f74255af59969207
Alican Dağıdır <alicandagidir@hotmail.com>
2023-11-02 20:56:11 +0300

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Property_Practice
+{
+    public class Employee
+    {
+        private string TrIDNumber;
+
+        public string TRIDNUMBER
+        {
+            get
+            {
+                return TrIDNumber.Substring(0, 5)+"******";
+            }
+            set
+            {
+                bool control = false;
+
+                if (value.Length == 11)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool IsNumber = char.IsNumber(value[i]);
+
+                        if (IsNumber)
+                        {
+                            //Don't need to do anything, because it is true
+                        }
+                        else
+                        {
+                            control = true;
+                            break;
+                        }
+
+                    }
+                    if (control)
+                    {
+                        Console.WriteLine("Please enter a valid TR ID Number");
+                    }
+                    else
+                    {
+                        TrIDNumber = value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The length of your input is not equal to 11, please edit it");
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Property_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+
+            e1.TRIDNUMBER = "12345678904";
+
+            Console.WriteLine("Your TR ID Number : " +e1.TRIDNUMBER);
+        }
+    }
+}
